a69c5363856c12723e5c320318f4e114265ab46e
dpismenny <denis.pismenny@gmail.com>
2013-12-23 14:15:16 +0200

    minor fix

--- js/common.js
+++ js/common.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-	$(".js-scrollable .js-scrollable__container").scrollable();
 
-	$('.js-toggleaccount').click(function(event) {
-		$('.account').slideToggle();
+	// scrollable
+	if( $('.js-scrollable').length ){
+		$('.js-scrollable .js-scrollable__container').scrollable();
+	}
+
+	// show account block
+	var $toggleAccount = $('.js-toggleaccount');
+	var $account = $('.account');
+	$($toggleAccount).click(function(event) {
+		$($account).slideToggle();
 	});
+
+	// show more pictures in .photo
+	var $photoMore = $('.photo__more');
+	var $photoPic = $('.photo__pic');
+	$($photoMore).click(function() {
+		$(this).parent().find($photoPic).removeClass('is-hidden');
+		$(this).addClass('is-hidden');
+		return false;
+	});
+
 });
