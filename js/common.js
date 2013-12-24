0da787604aa99270438e4044a93cf023e86412c0
dpismenny <denis.pismenny@gmail.com>
2013-12-24 14:16:14 +0200

    popup page

--- js/common.js
+++ js/common.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 	// show account block
 	var $toggleAccount = $('.js-toggleaccount');
 	var $account = $('.account-widget');
+
 	$($toggleAccount).click(function(event) {
 		$($account).slideToggle();
 	});
@@ -15,10 +16,45 @@ $(document).ready(function() {
 	// show more pictures in .photo
 	var $photoMore = $('.photo__more');
 	var $photoPic = $('.photo__pic');
+
 	$($photoMore).click(function() {
 		$(this).parent().find($photoPic).removeClass('is-hidden');
 		$(this).addClass('is-hidden');
 		return false;
 	});
+
+	// tabs
+	$('.js-tabs-item').removeClass('is-visible');
+	$('.js-tabs-nav li:first').addClass('is-active');
+	$('.js-tabs-item:first').addClass('is-visible');
+
+	$('.js-tabs-nav li').click(function() {
+		$('.js-tabs-nav li').removeClass('is-active');
+		$(this).addClass('is-active');
+		$('.js-tabs-item').removeClass('is-visible');
+		var activeTab = $(this).find('a').attr('href');
+		$(activeTab).addClass('is-visible');
+		return false;
+	});
+
+	// popup
+	var $popupOverlay = $('.overlay');
+	var $popup = $('.popup');
+
+	$('.js-login-btn').click(function() {
+		$($popup).addClass('is-active');
+		$($popupOverlay).addClass('is-active');
+		$('body').addClass('js-popup-open');
+		return false;
+	});
+	$($popupOverlay).click(function() {
+		$($popup).removeClass('is-active');
+		$('body').removeClass('js-popup-open');
+		$($popupOverlay).removeClass('is-active');
+		return false;
+	});
+
+
+
 
 });
